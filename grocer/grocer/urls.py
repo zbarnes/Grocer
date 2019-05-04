5ce2027b7a2f92b569b5c67b17978bba78a14653
qwergram <npengra317@gmail.com>
2019-05-04 12:56:43 -0700

    update settings to include demo app

--- grocer/grocer/urls.py
+++ grocer/grocer/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp.views import DemoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', DemoView.as_view()),
 ]
