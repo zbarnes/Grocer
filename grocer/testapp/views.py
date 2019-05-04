444691754c77d6df123912061809bf7380e6f264
qwergram <npengra317@gmail.com>
2019-05-04 12:58:46 -0700

    static files

--- grocer/testapp/views.py
+++ grocer/testapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class DemoView(TemplateView):
+    template_name = "testapp/test.html"
