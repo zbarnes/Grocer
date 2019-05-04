e9d1d5adbb02df5765406404c88dd9781e6dcd92
qwergram <npengra317@gmail.com>
2019-05-04 14:45:43 -0700

    dummy json

--- grocer/testapp/views.py
+++ grocer/testapp/views.py
@@ -5,3 +5,19 @@ from django.views.generic.base import TemplateView
 
 class DemoView(TemplateView):
     template_name = "testapp/test.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data()
+
+        context['tabs'] = [
+            {
+                'name': 'Example',
+                'blurb': 'ahhhh'
+            },
+            {
+                'name': 'Example2',
+            }
+        ]
+        context['note'] = "You are protected by the Good Smaritan Law!"
+
+        return context
